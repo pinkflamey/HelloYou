a520827d5c31387a24f21961654aa0d99764c9c3
pinkflamey <mavis@pinkflamey.nl>
2021-10-20 11:32:25 +0200

    worked on story logic

--- python/story.py
+++ python/story.py
@@ -1,38 +1,17 @@
 from getText import *
+from slowPrint import *
 
 
 # Story sections
 # ----------------------------
 
 def t1():
-    print(getText("txts/" + "t1.txt"))
-    input()
+    print("\n" + "Sectie 1".center(100, "-") + "\n")
+    slowPrint(getText("txts/" + "t1.txt") + "\n", 0.01, 0.08)
+    input("\n[Druk op enter om door te gaan]")
     q1()
 
-def t2():
-    print(getText("txts/" + "t2" + ".txt"))
-    input()
-    q2()
 
-def t2():
-    print(getText("txts/" + "t2" + ".txt"))
-    input()
-    q2()
-
-def t2():
-    print(getText("txts/" + "t2" + ".txt"))
-    input()
-    q2()
-
-def t2():
-    print(getText("txts/" + "t2" + ".txt"))
-    input()
-    q2()
-
-def t2():
-    print(getText("txts/" + "t2" + ".txt"))
-    input()
-    q2()
 
 
 
@@ -40,23 +19,25 @@ def t2():
 # ----------------------------
 
 def q1():
-    print(getText("txts/" + "q1" + ".txt"))
-    a = input("Antwoord: ")
-    if a == "a" or a == "A":
-        t2()
-    elif a == "b" or a == "B":
-        t3()
-    elif a == "c" or a == "C":
-        t4()
-    else:
+    print("\n" + "Vraag 1".center(100, "-") + "\n")
+    slowPrint(getText("txts/" + "q1" + ".txt") + "\n\n", 0.01, 0.08)
+
+    slowPrint("A. Ja".center(100), 0.01, 0.02)
+    slowPrint("B. Nee".center(100), 0.01, 0.02)
+    slowPrint("C. Ik ga niet".center(100), 0.01, 0.02)
+
+    a = ""
+    
+    while a == "":
+        a = input("\n\nAntwoord:")
+        if a == "a" or a == "A":
+            t2()
+        elif a == "b" or a == "B":
+            t3()
+        elif a == "c" or a == "C":
+            t4()
+        else:
+            print("Voer A, B of C in.")
         print("Voer A, B of C in.")
 
-def q2():
-    print(getText("txts/" + "q2" + ".txt"))
-    a = input("Antwoord: ")
-    if a == "a" or a == "A":
-        t5()
-    elif a == "b" or a == "B":
-        t6()
-    else:
-        print("Voer A of B in.")
+input()
