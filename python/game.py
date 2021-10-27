6bfb65381ff4e64e14f51fc47b5c3b5b92e86f27
pinkflamey <mavis@pinkflamey.nl>
2021-10-27 10:04:21 +0200

    added explanation and credits, and cleaned up code

--- python/game.py
+++ python/game.py
@@ -3,8 +3,6 @@ from sys import platform, stdout
 from time import sleep
 
 
-
-
 #-------------------------------------------
 # Logic Functions
 from getText import *
@@ -12,8 +10,6 @@ from slowPrint import *
 from printLines import *
 from clearScreen import *
 from changeScreenSize import *
-
-
 
 
 # "Waiting" animation with dots. Allows control over the number of dots in each cycle, the amount of cycles, and the time between each dot.
@@ -56,10 +52,39 @@ loading = "[»-(¯`·.·´¯)->...LOADING...<-(¯`·.·´¯)-«]".center(50)
 slowPrint(loading, 0.01, 0.13)
 slowPrint("...", 0.8, 0.8)
 
+changeScreenSize(100, 25)
 
+
+#-------------------------------------------
+# Explanation
+
+print('')
+print("Uitleg".center(100, "-"))
+sleep(0.5)
+print('\n\n')
+print('- Het spel bestaat uit stukjes verhaal ("secties") en vragen;'.center(100))
+sleep(1.5)
+print('- Bij elke vraag kun je A, B en soms C antwoorden;'.center(100))
+sleep(1.5)
+print('- De keuzes die je maakt beïnvloeden hoe het verhaal verloopt. Maak je keuzes aandachtig.'.center(100))
+sleep(2.5)
+
+x = input('\n\n\n\n\n\nOm het spel te starten, druk op [ENTER]. Om de credits te bekijken, typ "credits".\n')
 
 clearScreen()
 
+if x.lower() == "credits":
 
+    print('\n')
+    print('Credits'.center(100, "-"))
+    print('\n\nDit keuzeverhaal spel is gemaakt door Mavis de Ridder uit klas SD1C van het Mediacollege Amsterdam, als eind-beroepsopdracht voor periode 1, jaar 1.')
+    print('\n\nDe GitHub link voor dit project is: [https://github.com/pinkflamey/HelloYou]')
 
-t1()
+    input('Druk op [ENTER] om het spel te starten...')
+    clearScreen()
+    t1()
+    
+else:
+
+    clearScreen()
+    t1()
